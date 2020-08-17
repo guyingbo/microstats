aa038cd5dcf2da41055273ee63b74d8b2da7b6d0
Gu Yingbo <tensiongyb@gmail.com>
2020-08-17 15:02:54 +0800

    make cython optional

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ except ImportError:
     from distutils.core import setup
 import os.path
 import re
-
-from Cython.Build import cythonize
+import sys
 
 VERSION_RE = re.compile(r"""__version__ = ['"]([0-9.]+)['"]""")
 BASE_PATH = os.path.dirname(__file__)
@@ -22,6 +21,28 @@ with open(os.path.join(BASE_PATH, "README.md")) as readme:
     long_description = readme.read()
 
 
+ext_modules = None
+if (
+    not any(arg in sys.argv for arg in ["clean", "check"])
+    and "SKIP_CYTHON" not in os.environ
+):
+    try:
+        from Cython.Build import cythonize
+    except ImportError:
+        pass
+    else:
+        # For cython test coverage install with `make build-cython-trace`
+        compiler_directives = {}
+        if "CYTHON_TRACE" in sys.argv:
+            compiler_directives["linetrace"] = True
+        os.environ["CFLAGS"] = "-O3"
+        ext_modules = cythonize(
+            "microstats.py",
+            nthreads=int(os.getenv("CYTHON_NTHREADS", 0)),
+            language_level=3,
+            compiler_directives=compiler_directives,
+        )
+
 setup(
     name="microstats",
     description="A very simple in-memory statistics module",
@@ -36,7 +57,7 @@ setup(
     url="https://github.com/guyingbo/microstats",
     py_modules=["microstats"],
     python_requires=">=3.6",
-    ext_modules=cythonize("microstats.py", language_level=3),
+    ext_modules=ext_modules,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
